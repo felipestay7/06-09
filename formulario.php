9b7cc681485ceaf4836a1412e20649f214092c4b
felipestay7 <felipeulc@live.cl>
2017-09-06 23:27:34 -0300

    clase 06/09

---
+++ formulario.php
@@ -0,0 +1,43 @@
+<html>
+
+<head>
+
+<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+</head>
+
+<body>
+
+<div class="container">
+     <div class="row">
+          <div class="col-md-6 well">
+               <h3>Alumno</h3>
+	           <form action='procesar.php' method="GET">
+				    Nombre: <input type="text" name="nombre">
+				    <br>
+				    Apellido: <input type="text" name="apellido">
+				    <br>
+				    Rut: <input type="text" name="rut">
+				    <br>
+				    <input type="hidden" name="alumno_formulario">
+				    <input type="text" value="enviar" class="btn btn-success">
+               </form>
+          </div>
+          <div class="col-md-6 well">
+          	   <h3>Ramo</h3>
+               <form action='procesar.php' method="GET">
+					 Ramo: <input type="text" name="ramo">
+					 <br>
+					 Nota: <input type="text" name="nota">
+					 <br>
+					 <input type="hidden" name="ramo_formulario">
+					 <input type="submit" value="enviar" class="btn btn-danger">
+               </form>
+          </div>
+     </div>
+</div>        
+
+
+			<script type="text/javascript" src="js/query.js"></script>
+			<script type="text/javascript" src="js/bootstrap.js"></script>
+</body>
+</html>
---
+++ procesar.php
@@ -0,0 +1,14 @@
+<?php
+
+if (isset($_REQUEST['alumno_formulario'])){
+	$nombre = $_REQUEST['nombre'];
+	$apellido = $_REQUEST['apellido'];
+	$rut = $_REQUEST['rut'];
+}
+
+if (issset($_REQUEST['ramo_formulario'])){
+	$ramo = $_REQUEST['ramo'];
+    $nota = $_REQUEST['nota'];
+}
+
+?>
