5ba45860525fd2aa7d343c7958fe85ba976c888c
felipestay7 <31390022+felipestay7@users.noreply.github.com>
2017-09-13 17:20:32 -0300

    cambio

---
+++ ajax.php
@@ -0,0 +1,5 @@
+<?php
+
+$juanito = $_POST['juanito'];
+echo juanito;
+?>
--- formulario.php
+++ formulario.php
@@ -35,11 +35,27 @@
 					 <input type="submit" value="enviar" class="btn btn-danger">
                </form>
           </div>
+          <div class='row'>
+          	<div class="caol-md-12">
+          		<button class="btn btn-success btn-lg" onclick="procesarDatoAlumno()">Consulta Alumnos</button>
+          		</div>
      </div>
 </div>        
 
 
-			<script type="text/javascript" src="js/query.js"></script>
+			<script type="text/javascript" src="js/jquery.min.js"></script>
 			<script type="text/javascript" src="js/bootstrap.js"></script>
+			<script> function procesarDatoAlumno() {
+				$.ajax({
+					method: "POST",
+					url: "ajax.php",
+					data: { juanito: "John"}
+                })
+                    .done(function( msg) {
+                    	alert( "Data Saved: " + msg);
+                    });
+
+			}
+		</script>script>
 </body>
 </html>
