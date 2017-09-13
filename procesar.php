b860d2ac4f33a26c664074f249945a891ae4981c
felipestay7 <31390022+felipestay7@users.noreply.github.com>
2017-09-13 15:59:56 -0300

    cambio

--- procesar.php
+++ procesar.php
@@ -1,9 +1,26 @@
 <?php
 
+require 'conexion.php';
+
+function GuardarFormularioAlumno($nombre, $apellido, $rut)
+{
+    $sql = "INSERT INTO alumno VALUES(?,?,?)";
+    $smt = $conn->prepare($sql);
+    $smt->bindParam(1, $nombre);
+    $smt->bindParam(1, $apellido);
+    $smt->bindParam(1, $rut);
+    $smt->execute();
+    $resultado = $smt->fetchALL();
+    $conn = null;
+    return $resultado;
+}
+
 if (isset($_REQUEST['alumno_formulario'])){
 	$nombre = $_REQUEST['nombre'];
 	$apellido = $_REQUEST['apellido'];
 	$rut = $_REQUEST['rut'];
+
+	guardarFormularioAlumno($nombre, $apellido, $rut);
 }
 
 if (issset($_REQUEST['ramo_formulario'])){
